b6a094aa3dd5cb7c97c2354c07b2a8a823742a4e
Ibby <33922797+SleepySwords@users.noreply.github.com>
2024-09-22 15:23:57 +1000

    feat: add the `:Mathjax` and seperate the function

--- lua/mathjax/init.lua
+++ lua/mathjax/init.lua
@@ -19,7 +19,67 @@ function M.setup(new_config)
 	config = vim.tbl_extend("force", config, new_config)
 end
 
---- Setup this plugin
+--- Creates a job to render an image and put the image on the screen for this node
+--- @param matched_node TSNode
+--- @param con Config
+local function handle_matched_node(matched_node, con)
+	local latex_text = vim.treesitter.get_node_text(matched_node, 0, {})
+	local range = vim.treesitter.get_range(matched_node, 0, nil)
+	local height = range[4] - range[1] + 1
+	latex_text = string.gsub(latex_text, "%$%$", "")
+	for line in string.gmatch(latex_text:lower(), "%% lines: (%d*)") do
+		local line_parsed = tonumber(line)
+		if line_parsed ~= nil then
+			height = line_parsed
+		end
+	end
+	-- FIXME: Use this hash to see if we need to replace or not.
+	local num = vim.fn.sha256(latex_text .. con.color)
+	local url = globals.temp_dir .. "/" .. tostring(num) .. ".png"
+	local curr_win = vim.api.nvim_get_current_win()
+	local curr_buf = vim.api.nvim_get_current_buf()
+	Job:new({
+		command = "node",
+		args = { "index.js", url, con.color, latex_text },
+		cwd = mathjax_dir,
+		raw_args = true,
+		on_exit = function(_result, _r)
+			local image = api.from_file(url, {
+				window = curr_win,
+				buffer = curr_buf,
+				with_virtual_padding = true,
+				inline = true,
+				x = 0,
+				height = height,
+				y = range[4],
+			})
+			vim.schedule(function()
+				if image ~= nil then
+					local has_rendered = true
+					image:render() -- render image
+					globals.stale_images[image.id] = false
+					vim.api.nvim_create_autocmd("BufEnter", {
+						callback = function()
+							if
+								globals.stale_images[image.id] == false
+								and has_rendered == false
+								and vim.api.nvim_get_current_buf() == curr_buf
+							then
+								image:render()
+								has_rendered = true
+							else
+								image:clear()
+								has_rendered = false
+							end
+						end,
+					})
+				end
+			end)
+		end,
+	}):start()
+end
+
+--- Render the latex for this buffer.
 --- @param local_config? Config
 function M.render_latex(local_config)
 	local con
@@ -48,63 +108,14 @@ function M.render_latex(local_config)
 		for _, nodes in pairs(match) do
 			-- local capture = query_function.captures[id]
 			for _, matched_node in ipairs(nodes) do
-				local latex_text = vim.treesitter.get_node_text(matched_node, 0, {})
-				local range = vim.treesitter.get_range(matched_node, 0, nil)
-				local height = range[4] - range[1] + 1
-				latex_text = string.gsub(latex_text, "%$%$", "")
-				for line in string.gmatch(latex_text, "%% Lines: (%d*)") do
-					local line_parsed = tonumber(line)
-					if line_parsed ~= nil then
-						height = line_parsed
-					end
-				end
-				-- FIXME: Use this hash to see if we need to replace or not.
-				local num = vim.fn.sha256(latex_text .. con.color)
-				local url = globals.temp_dir .. "/" .. tostring(num) .. ".png"
-				local curr_win = vim.api.nvim_get_current_win()
-				local curr_buf = vim.api.nvim_get_current_buf()
-				Job:new({
-					command = "node",
-					args = { "index.js", url, con.color, latex_text },
-					cwd = mathjax_dir,
-					raw_args = true,
-					on_exit = function(_result, _r)
-						local image = api.from_file(url, {
-							window = curr_win,
-							buffer = curr_buf,
-							with_virtual_padding = true,
-							inline = true,
-							x = 0,
-							height = height,
-							y = range[4],
-						})
-						vim.schedule(function()
-							if image ~= nil then
-								local has_rendered = true
-								image:render() -- render image
-								globals.stale_images[image.id] = false
-								vim.api.nvim_create_autocmd("BufEnter", {
-									callback = function()
-										if
-											globals.stale_images[image.id] == false
-											and has_rendered == false
-											and vim.api.nvim_get_current_buf() == curr_buf
-										then
-											image:render()
-											has_rendered = true
-										else
-											image:clear()
-											has_rendered = false
-										end
-									end,
-								})
-							end
-						end)
-					end,
-				}):start()
+				handle_matched_node(matched_node, con)
 			end
 		end
 	end
 end
+
+vim.api.nvim_create_user_command("Mathjax", function()
+	M.render_latex()
+end, { desc = "Render the mathjax blocks in the current buffer." })
 
 return M
